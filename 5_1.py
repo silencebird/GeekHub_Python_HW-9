8f0912a120e8763fe27fb44d284870c65646edb1
Serhii Yefimenko <yefimenkoserhii@gmail.com>
2018-01-20 01:27:15 +0200

    optimaized homework#9

--- 5_1.py
+++ 5_1.py
@@ -2,139 +2,188 @@ import requests
 import argparse
 import logging
 import logging.config
+import datetime
+import time
 import json
 import pickle
 import os
+import re
 from config import *
 from jinja2 import Environment, FileSystemLoader
 
-
-if not (os.path.exists('results')):
+if not(os.path.exists('results')):
     os.makedirs('results')
 
-dictLogConfig = {
-    "version": 1,
-    "handlers": {
-        "fileHandler": {
-            "class": "logging.FileHandler",
-            "formatter": "myFormatter",
-            "filename": './' + RESULTS + '/' + LOG_FILE
-        }
-    },
-    "loggers": {
-        "Parser": {
-            "handlers": ["fileHandler"],
-            "level": "INFO",
-        }
-    },
-    "formatters": {
-        "myFormatter": {
-            "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        }
-    }
-}
+tag = re.compile(r'<.*s?>')
 
+class Logger(object):
 
-class HackerNews(object):
-    def __init__(self, category_url):
+    def __init__(self, config):
+        self.init_logger(config)
         self.logger = logging.getLogger("Parser")
-        self.article_data = {}
-        self.article_id = []
-        self.category_url = category_url
+        self.lvls = {
+            'critical': 50,
+            'error': 40,
+            'warning': 30,
+            'info': 20,
+            'debug': 10,
+            'notset': 0
+        }
 
-    # launch logger
-    def start_logger(self, dictLogConfig):
-        logging.config.dictConfig(dictLogConfig)
-        self.logger.info("Program started")
+    def init_logger(self, config):
+        logging.config.dictConfig(config)
 
-    # parse category using argparse, default = 'all'
-    def parse_category(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--category', default=DEFAULT_CATEGORY, choices=CATEGORIES + ['all'])
-        self.logger.info("Choose category %s" % parser.parse_args().category)
-        if parser.parse_args().category == 'all':
-            for category in CATEGORIES:
-                self.get_category_data(category)
+    def write_log(self, log_type, message):
+        self.logger.log(self.lvls[log_type], message)
+
+
+class Argparse(object):
+
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+
+    def add_args(self, default, choices):
+        self.parser.add_argument('--category', default=default, choices=choices)
+
+    def get_args(self, choices):
+        if self.parser.parse_args().category == 'all':
+            return choices[:-1]
         else:
-            self.get_category_data(parser.parse_args().category)
+            return self.parser.parse_args().category
 
-    # get article unique articles id's
-    def get_articles_id(self, current_ids):
-        if os.path.isfile('./data.pkl') and not self.article_id:
-            pkl_file = open('./data.pkl', 'rb')
-            self.article_id = pickle.load(pkl_file)
-            pkl_file.close()
-            self.article_id = list(set(self.article_id) | set(current_ids))
-            return self.article_id
 
-        if not self.article_id:
-            self.article_id.extend(current_ids)
-            return self.article_id
+
+class Parser(object):
+
+    def __init__(self):
+        self.response = ''
+
+    def parse_data(self, url, params, timeout):
+        self.response = requests.get(url, params=params, timeout=timeout)
+
+    def encode_response(self, text_format):
+        self.response.encoding = text_format
+
+    def get_resonse_content(self):
+        return json.loads(self.response.content)
+
+
+class Archive(object):
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.article_list = {}
+
+    def clear_article_list(self):
+        self.article_list = {}
+
+    def save_data(self, data):
+        output = open('{}.pkl'.format(self.file_name), 'wb')
+        pickle.dump(data, output)
+        output.close()
+
+    def load_data(self):
+        pkl_file = open('{}.pkl'.format(self.file_name), 'rb')
+        data = pickle.load(pkl_file)
+        pkl_file.close()
+        return data
+
+    def update_archive(self, data):
+        if not(os.path.isfile('{}.pkl'.format(self.file_name))):
+            self.save_data(data)
+            return data
         else:
-            new_ids = list(set(current_ids) - set(self.article_id))
-            self.article_id.extend(new_ids)
-            output = open('./data.pkl', 'ab')
-            pickle.dump(self.article_id, output)
-            output.close()
-            return new_ids
+            new_data = self.data_compare(data)
+            archive_data = self.load_data()
+            self.save_data(archive_data + new_data)
+            return new_data
+
+    def data_compare(self, data):
+        archive_data = self.load_data()
+        new_data = list(set(data) - set(archive_data))
+        return new_data
 
 
-    # get data from hacker news by article id
-    def get_category_data(self, category):
-        payload = {'print': 'pretty'}
-        self.logger.info(REQUEST_URL_CATEGORY + category + '.json')
-        try:
-            response = requests.get(self.category_url + category + '.json',
-                                    params=payload, timeout=5)
-        except ValueError:
-            self.logger.error("Request %s error" % ValueError)
-            print("Oops! Something wrong. Try again... ", ValueError)
+class HTMLCreator(object):
 
-        response.encoding = 'UTF-8'
-        #Python articles ids serialization
-        ids = self.get_articles_id(json.loads(response.content))
-        self.logger.info("Got request %s" % response.content)
-        self.logger.info("Create a report file %s" % REPORT)
-        self.logger.info('Start filter articles(not older - %s; score >= %s)' % (FROM_DATE_DEFAULT, SCORE_DEFAULT))
-        # Looping over articles
-        index = 0
-        print(len(ids))
-        for i in ids:
-            index += 1
-            print(index)
-            try:
-                article_text = (requests.get(REQUEST_URL_ARTICLE + str(i) + '.json', params=payload, timeout=5))
-            except ValueError:
-                self.logger.error("Request %s error" % ValueError)
-                print("Oops! Something wrong. Try again...", ValueError)
-            current_article = json.loads(article_text.content)
-            if datetime.datetime.fromtimestamp(current_article['time']) >= FROM_DATE_DEFAULT:
-                    if (current_article['score']) >= SCORE_DEFAULT:
-                        current_article['time'] = str(datetime.datetime.fromtimestamp(current_article['time']))
-                        self.article_data.setdefault(category, []).append(current_article)
-
-
-    # Creating HTML file from template using jinja2
-    def render_template(self, template_filename, context):
+    def render_template(self, template_filename, template_folder, context):
         path = os.path.dirname(os.path.abspath(__file__))
         template_environment = Environment(
             autoescape=False,
-            loader=FileSystemLoader(os.path.join(path, 'templates')),
+            loader=FileSystemLoader(os.path.join(path, '{}'.format(template_folder))),
             trim_blocks=False)
         return template_environment.get_template(template_filename).render(context)
 
-    def create_index_html(self):
-        fname = "output.html"
+    def create_html_file(self, output_file_name, template_file, template_folder, data):
+        output_file = '{}.html'.format(output_file_name)
         context = {
-            'articles': self.article_data
+            'articles': data
         }
-        with open(fname, 'w', encoding="utf-8") as f:
-            html = self.render_template('index.html', context)
+        with open(output_file, 'w', encoding="utf-8") as f:
+            html = self.render_template('{}.html'.format(template_file), template_folder, context)
             f.write(html)
 
 
-articles = HackerNews(REQUEST_URL_CATEGORY)
-articles.start_logger(dictLogConfig)
-articles.parse_category()
-articles.create_index_html()
-print(articles.article_data)
+class Article(object):
+
+    def __init__(self, filter_pattern):
+        self.filter_pattern = filter_pattern
+        self.article_content = dict.fromkeys([
+           "id", "by", "deleted", "text", "dead", "parent", "descendants", "kids",
+           "score", "time", "title", "type", "url", "parts", "poll"
+        ])
+
+    def update_content(self, article_data):
+        for key, value in article_data.items():
+            if key == 'text':
+                self.article_content[key] = self.filter_pattern.sub('', value)
+            elif key == 'time':
+                self.article_content[key] = datetime.datetime.fromtimestamp(value)
+            else:
+                self.article_content[key] = value
+
+
+if __name__ == "__main__":
+    archive = Archive(ARCHIVE_FILENAME)
+    archive.clear_article_list()
+    html_creator = HTMLCreator()
+    logger = Logger(DICT_LOG_CONFIG)
+    logger.write_log('info', "Program started")
+    args_parser = Argparse()
+    args_parser.add_args(DEFAULT_CATEGORY, CATEGORIES)
+    logger.write_log('info', 'Choose category {}'.format(args_parser.get_args(CATEGORIES)))
+    logger.write_log('info', 'Get data from: {}{}.json'.format(REQUEST_URL_CATEGORY, args_parser.get_args(CATEGORIES)))
+    categories = args_parser.get_args(CATEGORIES)
+    if not(type(categories) is list):
+        categories = [categories]
+    parser = Parser()
+
+    for category in categories:
+        try:
+            parser.parse_data('{}{}.json'.format(REQUEST_URL_CATEGORY, category), PAYLOAD, 5)
+        except ValueError:
+            print('Oops! Something wrong. Try again...{}'.format(ValueError))
+            logger.write_log('error', 'Request error {}'.format(ValueError))
+
+        parser.encode_response('UTF-8')
+        articles_ids = parser.get_resonse_content()
+        articles_ids = archive.update_archive(articles_ids)
+        for article_id in articles_ids:
+            try:
+                parser.parse_data('{}{}.json'.format(REQUEST_URL_ARTICLE, article_id), PAYLOAD, 5)
+            except ValueError:
+                print('Oops! Something wrong. Try again...{}'.format(ValueError))
+                logger.write_log('error', 'Request error {}'.format(ValueError))
+            current_article = parser.get_resonse_content()
+            if current_article:
+                if datetime.datetime.fromtimestamp(current_article['time']) >= FROM_DATE_DEFAULT:
+                    if (current_article['score']) >= SCORE_DEFAULT:
+                        parser.encode_response('UTF-8')
+                        article = Article(tag)
+                        article.update_content(current_article)
+                        archive.article_list.setdefault(category, []).append(article.article_content)
+
+    html_creator.create_html_file(
+        time.strftime("%Y%m%d-%H%M%S"), 'template', 'templates', archive.article_list
+    )
+
--- config.py
+++ config.py
@@ -1,12 +1,34 @@
 import datetime
 
-CATEGORIES = ['askstories', 'showstories', 'newstories', 'jobstories']
-DEFAULT_CATEGORY = 'all'
+CATEGORIES = ('askstories', 'showstories', 'newstories', 'jobstories', 'all')
+DEFAULT_CATEGORY = 'jobstories'
 LOG_FILE = 'hn_parser.log'
 RESULTS = 'results'
 REPORT = 'report.csv'
-FROM_DATE_DEFAULT = datetime.datetime.now() - datetime.timedelta(hours=0.5)
+FROM_DATE_DEFAULT = datetime.datetime.now() - datetime.timedelta(hours=0.2)
 SCORE_DEFAULT = 0
 REQUEST_URL_CATEGORY = 'https://hacker-news.firebaseio.com/v0/'
 REQUEST_URL_ARTICLE = 'https://hacker-news.firebaseio.com/v0/item/'
-
+PAYLOAD = {'print': 'pretty'}
+ARCHIVE_FILENAME = 'pkl_archive'
+DICT_LOG_CONFIG = {
+        "version": 1,
+        "handlers": {
+            "fileHandler": {
+                "class": "logging.FileHandler",
+                "formatter": "myFormatter",
+                "filename": './'+RESULTS+'/'+LOG_FILE
+            }
+        },
+        "loggers": {
+            "Parser": {
+                "handlers": ["fileHandler"],
+                "level": "INFO",
+            }
+        },
+        "formatters": {
+            "myFormatter": {
+                "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+            }
+        }
+    }
